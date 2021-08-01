2247060f9bbdf4a5382b55a36bbcbb94af31698a
Jonathan Philipos <pavilion.vps@gmail.com>
2021-08-01 14:48:58 +1000

    Update network.js

--- network.js
+++ network.js
@@ -1,24 +1,24 @@
 export default {
-  id: 'cosmos-hub-testnet', // DEPRECATE, only used for Lunie extension, NOT CHAIN ID
-  name: 'Cosmos Stargate',
+  id: 'virtengine', // DEPRECATE, only used for Lunie extension, NOT CHAIN ID
+  name: 'VirtEngine Network',
   description:
-    'Cosmos is a network of independent parallel blockchains, powered by BFT consensus algorithms like Tendermint.',
+    'VirtEngine is a decentralized Cloud Marketplace.',
   logo: `logo.svg`,
-  website: 'https://cosmos.network',
-  apiURL: 'http://localhost:8010/proxy', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
-  rpcURL: 'ws://34.123.30.100:26657',
-  stakingDenom: 'MUON',
+  website: 'https://virtengine.com',
+  apiURL: 'http://3.24.151.42:1317', // use `npx lcp --proxyUrl http://34.123.30.100:1317`
+  rpcURL: 'ws://3.24.151.42:26657',
+  stakingDenom: 'VE',
   coinLookup: [
     {
-      viewDenom: 'MUON',
-      chainDenom: 'umuon',
+      viewDenom: 'VE',
+      chainDenom: 'uve',
       chainToViewConversionFactor: 1e-6,
       icon: `currencies/muon.png`,
     },
   ],
-  addressPrefix: 'cosmos',
-  validatorAddressPrefix: 'cosmosvaloper',
-  validatorConsensusaddressPrefix: 'cosmosvalcons', // needed to map validators from staking queries to the validator set
+  addressPrefix: 'virtengine',
+  validatorAddressPrefix: 'virtenginevaloper',
+  validatorConsensusaddressPrefix: 'virtenginevalcons', // needed to map validators from staking queries to the validator set
   HDPath: `m/44'/118'/0'/0/0`,
   lockUpPeriod: `3 days`,
   fees: {
@@ -26,7 +26,7 @@ export default {
       gasEstimate: 350000,
       feeOptions: [
         {
-          denom: 'MUON',
+          denom: 'VE',
           amount: 0.001,
         },
       ],
